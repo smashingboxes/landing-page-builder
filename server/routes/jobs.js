685b5c4f479e6580874464ca22c54248c035ec72
Joe Jackson <cpmhjoe@gmail.com>
2016-02-03 13:48:02 -0600

    Rename Workable Jobs To Clarify That It Is An Adpater

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -2,17 +2,17 @@
 
 const express = require('express');
 const WorkableJob = require('../models/workableJob');
-const workableJob = new WorkableJob();
+const jobAPI = new WorkableJob();
 const router = express.Router();
 
 router.get('/jobs', (_req, res, next) => {
-  workableJob.getPublishedJobs()
+  jobAPI.getPublishedJobs()
     .then(jobs => res.json({ data: jobs }))
     .catch(err => next(err));
 });
 
 router.get('/jobs/:slug', (req, res, next) => {
-  workableJob.getJobBySlug(req.params.slug)
+  jobAPI.getJobBySlug(req.params.slug)
     .then(job => res.json({ data: job }))
     .catch(err => next(err));
 });
