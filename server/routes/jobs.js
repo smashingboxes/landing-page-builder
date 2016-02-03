890f237e3ed06263432713205b479917433c91ca
Joe Jackson <cpmhjoe@gmail.com>
2016-02-03 11:41:13 -0600

    Adds An Individual Job Endpoint
    Per Growth, we've been asked to have the job posting urls in the format
    `/jobs/:slug` rather than `/jobs/:shortcode` this means that we litterally have
    to loop through every job and check against our generated slug for matches. This
    is not scalable but we will have to rethink things if we have 100s of job
    listings anyway.
    
    Caching has been pulled back to the model from the router, to allow the single
    jobs request to use the cached request for multiples.
    
    These requests are cached in the same manner.
    
    I also fixed a bug in the Redis caching scheme that would try to fall back to a
    null value if there was an error. The error is now passed on if the backup is a
    null value.

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const express = require('express');
-const redisClient = require('../utils/redisClient');
 const WorkableJob = require('../models/workableJob');
+const workableJob = new WorkableJob();
 const router = express.Router();
 
-router.get('/jobs', function(_req, res, next) {
-  const workableJob = new WorkableJob();
-  redisClient.cacheFetch('publishedjobs', workableJob.getPublishedJobs)
+router.get('/jobs', (_req, res, next) => {
+  workableJob.getPublishedJobs()
     .then(jobs => res.json({ data: jobs }))
+    .catch(err => next(err));
+});
+
+router.get('/jobs/:slug', (req, res, next) => {
+  workableJob.getJobBySlug(req.params.slug)
+    .then(job => res.json({ data: job }))
     .catch(err => next(err));
 });
 
--- spec/server/jobs_spec.js
+++ spec/server/jobs_spec.js
@@ -7,7 +7,7 @@ const app = require('../../server/app');
 const redisClient = require('../../server/utils/redisClient').client;
 const port = 3000;
 
-describe('GET /jobs', () => {
+describe('Job Endpoints', () => {
   const requestOptions = { json: true };
   let server;
 
@@ -30,27 +30,75 @@ describe('GET /jobs', () => {
     redisClient.flushdb(() => { done(); });
   });
 
-  it('returns a list of jobs', (done) => {
-    requestOptions.uri = 'http://localhost:3000/jobs';
-    request(requestOptions).then((body) => {
-      expect(body.data[0]).to.be.a('object');
-      done();
+
+  describe('GET /jobs', () => {
+    beforeEach((done) => {
+      nock.cleanAll();
+      mockWorkableJobs().replyWithFile(200, './spec/fixtures/workable_jobs.json');
+      mockWorkableJobs().replyWithError('should not have been called');
+      redisClient.flushdb(() => { done(); });
+    });
+
+    it('returns a list of jobs', (done) => {
+      requestOptions.uri = 'http://localhost:3000/jobs';
+      request(requestOptions).then((body) => {
+        expect(body.data[0]).to.be.a('object');
+        done();
+      });
+    });
+
+    it('caches the jobs list after the first request', (done) => {
+      requestOptions.uri = 'http://localhost:3000/jobs';
+      request(requestOptions)
+        .then(() => { return request(requestOptions); })
+        .then(() => { done(); })
+        .catch((err) => { done(err); });
+    });
+
+    it('includes a slug on the jobs', (done) => {
+      requestOptions.uri = 'http://localhost:3000/jobs';
+      request(requestOptions).then((body) => {
+        expect(body.data[0].slug).to.equal('product-director');
+        done();
+      });
     });
   });
 
-  it('caches the jobs list after the first request', (done) => {
-    requestOptions.uri = 'http://localhost:3000/jobs';
-    request(requestOptions)
-      .then(() => { return request(requestOptions); })
-      .then(() => { done(); })
-      .catch((err) => { done(err); });
-  });
+  describe('GET /jobs/:slug', () => {
+    const mockSingleWorkableJob = () => {
+      return nock('https://www.workable.com/spi/v3/accounts/smashingboxes')
+        .get('/jobs/A02AF3EE6C');
+    };
 
-  it('includes a slug on the jobs', (done) => {
-    requestOptions.uri = 'http://localhost:3000/jobs';
-    request(requestOptions).then((body) => {
-      expect(body.data[0].slug).to.equal('product-director');
-      done();
+    it('get\'s a job by slug', (done) => {
+      mockSingleWorkableJob().replyWithFile(200, './spec/fixtures/workable_job.json');
+      requestOptions.uri = 'http://localhost:3000/jobs/product-director';
+      request(requestOptions).then((body) => {
+        expect(body.data.title).to.equal('Product Director');
+        expect(body.data.full_description).to.contain('Product Director');
+        done();
+      });
+    });
+
+    it('caches the job after the first request', (done) => {
+      mockSingleWorkableJob().replyWithFile(200, './spec/fixtures/workable_job.json');
+      mockSingleWorkableJob().replyWithError('should not happen');
+      requestOptions.uri = 'http://localhost:3000/jobs/product-director';
+      request(requestOptions)
+        .then(() => { return request(requestOptions); })
+        .then(() => { done(); })
+        .catch((err) => { done(err); });
+    });
+
+    it('handles a non existant slug', (done) => {
+      mockSingleWorkableJob().reply(404);
+      requestOptions.uri = 'http://localhost:3000/jobs/awesome-director';
+      request(requestOptions)
+        .then(() => done(new Error('Should not have succeeded')))
+        .catch(err => {
+          expect(err.statusCode).to.equal(404);
+          done();
+        });
     });
   });
 
