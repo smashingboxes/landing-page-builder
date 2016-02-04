a82f109ab02ff1d9a936e366c66d57abd85b3400
Joe Jackson <cpmhjoe@gmail.com>
2016-02-04 15:58:54 -0600

    Mount the Jobs Router at /jobs
    Instead of mounting the jobs router at root and routing to /jobs in each
    individual route.  This will allow jobs to be mounted anywhere in the future.

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -5,13 +5,13 @@ const WorkableJob = require('../models/workableJob');
 const jobAPI = new WorkableJob();
 const router = express.Router();
 
-router.get('/jobs', (_req, res, next) => {
+router.get('/', (_req, res, next) => {
   jobAPI.getPublishedJobs()
     .then(jobs => res.json({ data: jobs }))
     .catch(err => next(err));
 });
 
-router.get('/jobs/:slug', (req, res, next) => {
+router.get('/:slug', (req, res, next) => {
   jobAPI.getJobBySlug(req.params.slug)
     .then(job => res.json({ data: job }))
     .catch(err => next(err));
